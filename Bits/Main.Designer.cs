aa1358724cf6244dbd276c7e9c306f97194d3e0a
haroruhomer <harmol22@hotmail.com>
2015-02-17 18:14:13 -0500

    Agregado

---
+++ Bits/Main.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Bits
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.CH_Grafico = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.TB_Bits = new System.Windows.Forms.TextBox();
+            this.L_Bits = new System.Windows.Forms.Label();
+            this.LB_Opciones = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CH_Grafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CH_Grafico
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.CH_Grafico.ChartAreas.Add(chartArea1);
+            this.CH_Grafico.Location = new System.Drawing.Point(12, 38);
+            this.CH_Grafico.Name = "CH_Grafico";
+            this.CH_Grafico.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            this.CH_Grafico.Size = new System.Drawing.Size(769, 443);
+            this.CH_Grafico.TabIndex = 0;
+            this.CH_Grafico.Text = "Paso de Bits";
+            // 
+            // TB_Bits
+            // 
+            this.TB_Bits.Location = new System.Drawing.Point(116, 12);
+            this.TB_Bits.Name = "TB_Bits";
+            this.TB_Bits.Size = new System.Drawing.Size(383, 20);
+            this.TB_Bits.TabIndex = 1;
+            this.TB_Bits.Text = "01001100011";
+            // 
+            // L_Bits
+            // 
+            this.L_Bits.AutoSize = true;
+            this.L_Bits.Location = new System.Drawing.Point(31, 19);
+            this.L_Bits.Name = "L_Bits";
+            this.L_Bits.Size = new System.Drawing.Size(79, 13);
+            this.L_Bits.TabIndex = 2;
+            this.L_Bits.Text = "Cadena de Bits";
+            // 
+            // LB_Opciones
+            // 
+            this.LB_Opciones.FormattingEnabled = true;
+            this.LB_Opciones.Items.AddRange(new object[] {
+            "NRZ-L",
+            "NRZI",
+            "Bipolar-AMI",
+            "Pseudoternario",
+            "Manchester",
+            "Manchester Diferencial",
+            "B8ZS",
+            "HDB3"});
+            this.LB_Opciones.Location = new System.Drawing.Point(787, 90);
+            this.LB_Opciones.Name = "LB_Opciones";
+            this.LB_Opciones.Size = new System.Drawing.Size(120, 108);
+            this.LB_Opciones.TabIndex = 3;
+            this.LB_Opciones.SelectedIndexChanged += new System.EventHandler(this.LB_Opciones_SelectedIndexChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 493);
+            this.Controls.Add(this.LB_Opciones);
+            this.Controls.Add(this.L_Bits);
+            this.Controls.Add(this.TB_Bits);
+            this.Controls.Add(this.CH_Grafico);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CH_Grafico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart CH_Grafico;
+        private System.Windows.Forms.TextBox TB_Bits;
+        private System.Windows.Forms.Label L_Bits;
+        private System.Windows.Forms.ListBox LB_Opciones;
+    }
+}
+
---
+++ Bits/Main.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Bits
+{
+    public partial class Main : Form
+    {
+        private Series datos=new Series();
+        private string cadenaBits;
+        private char[] bits;
+        public Main()
+        {
+            InitializeComponent();
+            datos.ChartType = SeriesChartType.StepLine;
+        }
+
+        private void LB_Opciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cadenaBits = TB_Bits.Text;
+            bits = new Char[cadenaBits.Length];
+            bits = cadenaBits.ToCharArray();
+            datos.Points.Clear();
+            switch (LB_Opciones.SelectedIndex)
+            {
+                case 0:
+                    nzrl();
+                    break;
+                case 1:
+                    nrzi();
+                    break;
+                case 2:
+                    bAmi();
+                    break;
+                case 3:
+                    pseudo();
+                    break;
+                case 4:
+                    manchester();
+                    break;
+                case 5:
+                    diferencial();
+                    break;
+                case 6:
+                    bzs();
+                    break;
+                case 7:
+                    hdb();
+                    break;
+            }
+            CH_Grafico.Series.Add("Bits");
+            CH_Grafico.Series[0].BorderWidth=3;
+            CH_Grafico.Series[0]=datos;
+            CH_Grafico.Series[0].XValueType = ChartValueType.Double;
+            CH_Grafico.Series[0].XValueMember = "puntos";
+            CH_Grafico.ChartAreas[0].AxisX.TextOrientation = TextOrientation.Rotated90;
+        }
+
+        private void hdb()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void bzs()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void diferencial()
+        {//siempre mitad del inter 0=Trans inicio del inter- 1 no trans al inicio 
+            
+            int aux = 1;
+            int aux2 = 0;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] == '0')
+                {
+                    if (aux == 0)
+                    {
+                        aux = 1;
+                        datos.Points.AddXY(i, aux);
+
+                    }
+                    else
+                    {
+                        aux = 0;
+                        datos.Points.AddXY(i, aux);
+                    }
+                    if (aux2 == 0)
+                        aux2 = 1;
+                    else
+                        aux2 = 0;
+                }
+                else
+                {
+                    if (aux2 == 0)
+                        aux2 = 1;
+                    else
+                        aux2 = 0;
+                }
+
+                datos.Points.AddXY(i + 0.5, aux2);
+            }
+
+            
+
+        }
+
+        private void manchester()
+        {//Medio intervalo 0 trancision alto bajo- 1 bajo alto 
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] == '0')
+                {
+                    datos.Points.AddXY(i, 1);
+                    datos.Points.AddXY(i + 0.5, 0);
+                }
+                else
+                {
+                    datos.Points.AddXY(i, 0);
+                    datos.Points.AddXY(i + 0.5, 1);
+                }
+            }
+            
+        }
+
+        private void pseudo()
+        {//alcontrario bipolar 
+            int aux = -1;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] != '1')
+                {
+                    aux *= -1;
+                    datos.Points.AddXY(i, aux);
+                }
+                else
+                    datos.Points.AddXY(i, 0);
+            }
+                
+        }
+
+        private void bAmi()
+        {//0 no señal- 1 Nivel + ó - alternante
+            int aux=-1;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i]!='0'){
+                    aux *= -1;
+                    datos.Points.AddXY(i, aux);
+                }
+                else
+                    datos.Points.AddXY(i, 0);
+            }           
+        }
+
+        private void nrzi()
+        {//0 no trancisión inicio de intervalo- 1 Transicion principio de intervalo
+            int aux = 0;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] == '1')
+                {
+                    if (aux == 0)
+                        aux = 1;
+                    else
+                        aux = 0;
+                }
+                datos.Points.AddXY(i, aux);
+            }
+        }
+
+        private void nzrl()
+        {//0 Nivel alto 1 Nivel Bajo
+            for(int i=0; i<bits.Length; i++){
+                if (bits[i] == '0')
+                {
+                    datos.Points.AddXY(i, 1);
+                }
+                else
+                {
+                    datos.Points.AddXY(i, 0);
+                }
+            }
+        }
+    }
+}
